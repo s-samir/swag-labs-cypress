365bbccee8166c43f2fcbce9a6130b13fdbd0f59
Enes <enesku@maestralsolutions.com>
2022-10-20 14:48:36 +0200

    feedback

--- cypress/integration/sauceDemoProject/test/pageObjects/checkoutPageObject.js
+++ cypress/integration/sauceDemoProject/test/pageObjects/checkoutPageObject.js
@@ -3,17 +3,16 @@ import { BasePageObject } from "./basePageObject";
 export class CheckoutPageObject extends BasePageObject {
     constructor(){
         super('testData');
+        //locators
         this.verifySecondaryContainerCheckoutInfo = e => cy.get(".header_secondary_container").contains('Checkout: Your Information');
         this.getCancelButton = e => cy.get("#cancel").should('be.visible').should('be.enabled');
         this.verifySecondaryContainerCheckoutOverview = e => cy.get(".header_secondary_container").contains('Checkout: Overview');
-        this.verifyItem4Price = e => cy.get('.item_pricebar').should('be.visible').contains('$29.99');
-        this.verifyItem0Price = e => cy.get('.item_pricebar').should('be.visible').contains('$9.99');
+        this.verifyItemPrice = e => cy.get('.item_pricebar').should('be.visible').contains(e);
         this.verifyItemTotalPrice = e => cy.get('.summary_info').contains('Item total: $39.98');
         this.verifyTaxPrice = e => cy.get('.summary_info').contains('Tax: $3.20');
         this.verifyTotalPrice = e => cy.get('.summary_info').contains('Total: $43.18');
         this.getQuantity = e => cy.get('.cart_quantity').contains('1');
-        this.verifyCheckoutTYMsg = e => cy.get('.checkout_complete_container').contains('THANK YOU FOR YOUR ORDER');
-        this.verifyCheckoutCompleteContent = e => cy.get('.checkout_complete_container').contains('Your order has been dispatched, and will arrive just as fast as the pony can get there!');
+        this.verifyCheckouMsg = e => cy.get('.checkout_complete_container').contains(e);
         this.getPonyExpressImg = e => cy.get('.pony_express').should('be.visible');
         this.getBackToHomeButton = e => cy.get('#back-to-products').should('be.visible').should('be.enabled');
         this.getContinueShoppingButton = e => cy.get("#continue-shopping").should('be.visible').should('be.enabled');
@@ -26,24 +25,25 @@ export class CheckoutPageObject extends BasePageObject {
         this.getPostalCodeField = e => cy.get('input[name=postalCode]').should('be.visible');
         this.verifyUrlCheckoutComplete = e => cy.url().should('contain', 'complete');
         
+        //actions
         
 
     }
 
     clickContinueShoppingButton() {
-        return this.getContinueShoppingButton().click();
+        this.getContinueShoppingButton().click();
     }
 
     clickCheckoutButton() {
-        return this.getCheckoutButton().click();
+        this.getCheckoutButton().click();
     }
 
     clickContinueButton() {
-        return this.getContinueButton().click();
+        this.getContinueButton().click();
     }
 
     clickFinishButton() {
-        return cy.get('#finish').should('be.visible').should('be.enabled').click();
-    }
+        cy.get('#finish').should('be.visible').should('be.enabled').click();
+    } 
     
 }
