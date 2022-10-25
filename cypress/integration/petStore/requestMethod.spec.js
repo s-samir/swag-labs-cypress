0382feab69e3c419022b74633585234ed982ff15
s-samir <samir.suljovic12@gmail.com>
2022-10-25 14:00:07 +0200

    updated spec file

--- cypress/integration/petStore/requestMethod.spec.js
+++ cypress/integration/petStore/requestMethod.spec.js
@@ -4,39 +4,53 @@ describe('Request method - PetStore',function() {
 
     it('Request method - CRUD', function() {
         // Navigate to the PetStore url:
-        cy.visit('https://petstore.swagger.io/')
+        cy.visit(Cypress.env('petStoreUrl'));
+        cy.url().should('eq', 'https://petstore.swagger.io/')
 
         // Add a new pet to the store:
-        cy.request('POST', 'https://petstore.swagger.io/v2/pet', { id: '258963147001', name: 'doggie', status: 'available' }).then(
+        cy.request('POST', Cypress.env('petUrl'), { 
+            id: Cypress.env('petId'), 
+            name: Cypress.env('petName'), 
+            status: Cypress.env('petStatus')
+        }).then(
         (response) => {
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('id', 258963147001)
-            expect(response.body).to.have.property('name', 'doggie')
+            expect(response.body).to.have.property('id', Cypress.env('petId'))
+            expect(response.body).to.have.property('name', Cypress.env('petName'))
+            expect(response.body).to.have.property('status', Cypress.env('petStatus'))
         }
         )
         // Find pet by id:
-        cy.request('GET', 'https://petstore.swagger.io/v2/pet/258963147001', { id: '258963147001' }).then(
+        cy.request('GET', Cypress.env('petUrl') + Cypress.env('petId'), { 
+            id: Cypress.env('petId') 
+        }).then(
         (response) => {
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('id', 258963147001)
-            expect(response.body).to.have.property('name', 'doggie')
-            expect(response.body).to.have.property('status', 'available')
+            expect(response.body).to.have.property('id', Cypress.env('petId'))
+            expect(response.body).to.have.property('name', Cypress.env('petName'))
+            expect(response.body).to.have.property('status', Cypress.env('petStatus'))
         }
         )
         // Update pet in the store with form data:
-        cy.request('POST', 'https://petstore.swagger.io/v2/pet?petId=258963147001', { id: '258963147001', name: 'snoop doggie', status: 'sold' }).then(
+        cy.request('POST', Cypress.env('petUrl') + "?petId=" + Cypress.env('petId'), { 
+            id: Cypress.env('petId'), 
+            name: Cypress.env('updatedPetName'), 
+            status: Cypress.env('updatedPetStatus') 
+        }).then(
         (response) => {
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('id', 258963147001)
-            expect(response.body).to.have.property('name', 'snoop doggie')
-            expect(response.body).to.have.property('status', 'sold')
+            expect(response.body).to.have.property('id', Cypress.env('petId'))
+            expect(response.body).to.have.property('name', Cypress.env('updatedPetName'))
+            expect(response.body).to.have.property('status', Cypress.env('updatedPetStatus'))
         }
         )
         // Delete pet:
-        cy.request('DELETE', 'https://petstore.swagger.io/v2/pet/258963147001', { id: '258963147001' }).then(
+        cy.request('DELETE', Cypress.env('petUrl') + Cypress.env('petId'), { 
+            id: Cypress.env('petId') 
+        }).then(
         (response) => {
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('message', '258963147001') // 'message' is actually 'id' of the pet.
+            expect(response.body).to.have.property('message', Cypress.env('petId').toString()) // 'message' is actually 'id' of the pet.
         }
         )
 
